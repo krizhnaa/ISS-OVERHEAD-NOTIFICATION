18cf9f4f6fc6765a0b9502f1ac791a7c02b1eae2
krizhnaa <krizhnaprem@gmail.com>
2024-01-14 10:43:24 +0530

    Done with gathering lat and long

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 > Building a notification system that sends an E-mail when the INTERNATIONAL SPACE STATION is near me.
-
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 9.320982
+MY_LONG = 76.547022
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -11,25 +11,33 @@ data = response.json()
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
-#Your position is within +5 or -5 degrees of the ISS position.
+iss_tuple = (iss_latitude, iss_longitude)
+myloc_tuple = (MY_LAT, MY_LONG)
 
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+def iss_near():
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG <= iss_longitude <= MY_LONG:
+        print("near me")
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-time_now = datetime.now()
+# parameters = {
+#     "lat": MY_LAT,
+#     "lng": MY_LONG,
+#     "formatted": 0,
+#     "tzid" : "Asia/Kolkata"
+# }
+#
+# response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+# response.raise_for_status()
+# data = response.json()
+# sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+# sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+# time_now = datetime.now()
+#
+# print(time_now)
 
 #If the ISS is close to my current position
-# and it is currently dark
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
 
