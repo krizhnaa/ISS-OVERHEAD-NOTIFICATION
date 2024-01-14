a88cea78e257aec3068204aa3d9dccd44a003222
krizhnaa <krizhnaprem@gmail.com>
2024-01-14 10:50:56 +0530

    Look Upgit add .!

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
-from datetime import datetime
+import smtplib
+import time
 
-MY_LAT = 9.320982
-MY_LONG = 76.547022
+MY_LAT = xxx
+MY_LONG = yyy
+
+my_mail = "krizhnatester@gmail.com"
+password = ""
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -16,30 +20,22 @@ myloc_tuple = (MY_LAT, MY_LONG)
 
 
 def iss_near():
-    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG <= iss_longitude <= MY_LONG:
-        print("near me")
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
 
 
-# parameters = {
-#     "lat": MY_LAT,
-#     "lng": MY_LONG,
-#     "formatted": 0,
-#     "tzid" : "Asia/Kolkata"
-# }
-#
-# response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-# response.raise_for_status()
-# data = response.json()
-# sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-# sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+while True:
+    time.sleep(60)
+    if iss_near():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_mail, password=password)
+            connection.sendmail(
+                from_addr=my_mail,
+                to_addrs="",
+                msg=f"Subject:ISS Overhead \n\nGO out and Look up, there is the ISS"
+            )
 
-# time_now = datetime.now()
-#
-# print(time_now)
-
-#If the ISS is close to my current position
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
 
 
 
